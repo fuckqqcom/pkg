3c57953e2c5066c993ce0b757504dca8b339cfb6
xiaohan <fuck@qq.com>
2024-03-19 17:04:31 +0800

    init

--- constantx/config.go
+++ constantx/config.go
@@ -4,6 +4,7 @@ const (
 	TenantId    = "tenant_id"
 	AppId       = "app_id"
 	UserId      = "user_id"
+	AccountId   = "account_id"
 	ArticleId   = "article_id"
 	Status      = "status"
 	IsDeleted   = "is_deleted"
