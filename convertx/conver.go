12ac484c01794ae75befacc692025a07e2dd0b43
xiaohan <fuck@qq.com>
2024-03-23 16:07:21 +0800

    DefaultVersion

--- convertx/conver.go
+++ convertx/conver.go
@@ -2,6 +2,7 @@ package convertx
 
 import (
 	"github.com/duke-git/lancet/v2/convertor"
+	"strings"
 )
 
 func AnyToInt64(value any) (int64, error) {
@@ -15,7 +16,30 @@ func AnyToArr[T comparable](arr []T) []any {
 	}
 	return t
 }
+func AnyToStr(value any) string {
+	return convertor.ToString(value)
+}
 
 func ToBytes(value any) ([]byte, error) {
 	return convertor.ToBytes(value)
+}
+
+func ContainIgnoreCase(target string, arr []string) bool {
+	target = strings.ToLower(target)
+
+	for _, item := range arr {
+		if strings.ToLower(item) == target {
+			return true
+		}
+	}
+	return false
+}
+
+func Contains[T comparable](target T, arr []T) bool {
+	for _, item := range arr {
+		if item == target {
+			return true
+		}
+	}
+	return false
 }
