fa1fafdea8d97da12bd8bb94a304a2b5e0650158
xiaohan <fuck@qq.com>
2024-03-23 15:13:07 +0800

    DefaultVersion

--- constantx/config.go
+++ constantx/config.go
@@ -1,14 +1,15 @@
 package constantx
 
 const (
-	Id         = "id"
-	TenantId   = "tenant_id"
-	AppId      = "app_id"
-	UserId     = "user_id"
-	AccountId  = "account_id"
-	ArticleId  = "article_id"
-	MaterialId = "material_id"
-	MediaId    = "media_id"
+	Id             = "id"
+	TenantId       = "tenant_id"
+	AppId          = "app_id"
+	UserId         = "user_id"
+	AccountId      = "account_id"
+	ArticleId      = "article_id"
+	MaterialId     = "material_id"
+	OrganizationId = "organization_id"
+	MediaId        = "media_id"
 
 	OpenStatus      = "open_status"
 	DeletedStatus   = "deleted_status"
