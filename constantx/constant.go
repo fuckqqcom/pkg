32c85d87d54fd2f46e4d160823bac33354943337
xiaohan <fuck@qq.com>
2024-03-23 20:11:09 +0800

    DefaultVersion

--- constantx/constant.go
+++ constantx/constant.go
@@ -3,9 +3,11 @@ package constantx
 const (
 	Default        = -1
 	Version        = 0
-	IsNotDeleted   = 1 //已删除
-	IsNotDrafted   = 1 //已推送
-	IsNotPublished = 1 //已发布
-	IsOpen         = 1 //开启
+	IsNotDeleted   = 1  //未删除
+	IsDeleted      = -1 //已删除
+	IsNotDrafted   = 1  //已推送
+	IsNotPublished = 1  //已发布
+	IsOpen         = 1  //开启
+	IsNotOpen      = -1
 	IsNotAudited   = 1
 )
