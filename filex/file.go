fe7c98a695fccdeca55f19a44e3475bd0cdd8919
fuck <fuck@qq.com>
2024-03-24 13:26:47 +0800

    MediaType

---
+++ filex/file.go
@@ -0,0 +1,29 @@
+package filex
+
+import (
+	"io"
+	"mime/multipart"
+	"os"
+	"path/filepath"
+)
+
+// Create 创建文件
+func Create(file *multipart.FileHeader, dst string) error {
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	if err = os.MkdirAll(filepath.Dir(dst), 0750); err != nil {
+		return err
+	}
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, src)
+	return err
+}
