835291e056f50bf1c60fe15a1f2bfa3472125ba9
xiaohan <fuck@qq.com>
2024-04-06 21:00:39 +0800

    default

--- constantx/constant.go
+++ constantx/constant.go
@@ -7,6 +7,8 @@ const (
 	IsDeleted      = -1 //已删除
 	IsNotDrafted   = -1 //未推送
 	IsDrafted      = 1
+	IsNotDefault   = -1
+	IsDefault      = 1
 	IsNotPublished = -1 //未发布
 	IsPublished    = 1
 	IsOpen         = 1 //开启
