bb35c8c6c8d929e82194fc0986b9d26ef3b894a1
xiaohan <fuck@qq.com>
2024-05-12 20:31:36 +0800

    LanguageModelId

---
+++ encryx/regexp.go
@@ -0,0 +1,16 @@
+package encryx
+
+import (
+	"regexp"
+)
+
+var emailRegexp = regexp.MustCompile("[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[\\w](?:[\\w-]*[\\w])?")
+var phoneRegexp = regexp.MustCompile("\\d{3}\\d{8}|\\d{4}\\{7,8}")
+
+func CheckEmail(email string) bool {
+	return emailRegexp.MatchString(email)
+}
+
+func CheckPhone(phone string) bool {
+	return phoneRegexp.MatchString(phone)
+}
