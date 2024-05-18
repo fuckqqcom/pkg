89a178008f649361bfbe6d3f43431665c2547f7b
xiaohan <fuck@qq.com>
2024-05-18 14:05:32 +0800

    user struct

--- metadatax/types.go
+++ metadatax/types.go
@@ -4,6 +4,4 @@ type UserInfo struct {
 	Id       int64 `json:"id"`
 	TenantId int64 `json:"tenant_id"`
 	AppId    int64 `json:"app_id"`
-	//OrganizationId int64 `json:"organization_id"`
-	SuperuserStatus int64 `json:"superuser_status"`
 }
