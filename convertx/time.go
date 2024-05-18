caf7d42cab565aadac1b66eee37f7ce67d7b7eb8
xiaohan <fuck@qq.com>
2024-05-18 13:08:21 +0800

    LanguageModelId

--- convertx/time.go
+++ convertx/time.go
@@ -11,15 +11,22 @@ func Unix() int64 {
 	return datetime.NewUnixNow().ToUnix()
 }
 
-func CompareInt(begin, end int64) bool {
+func CompareBetweenInt(begin, end int64) bool {
 	now := time.Now().In(loc).Unix()
 	return end > begin && begin < now && end > now
 }
-func CompareNow(begin, end time.Time) bool {
+func CompareBetweenNow(begin, end time.Time) bool {
 	now := time.Now().In(loc)
 	return end.After(begin) && begin.Before(now) && end.After(now)
 }
-
+func CompareNow(t time.Time) bool {
+	now := time.Now().In(loc)
+	return now.After(t)
+}
+func CompareInt(start int64) bool {
+	now := time.Now().In(loc).Unix()
+	return now > start
+}
 func Timestamp(timezone string) int64 {
 	return datetime.Timestamp(timezone)
 }
