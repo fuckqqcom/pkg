700ff8c5a2ab200d46f37910224c913c3a34a731
xiaohan <hanhongbing@people.cn>
2024-10-13 12:00:12 +0800

    init

---
+++ rule/count.go
@@ -0,0 +1,14 @@
+package rule
+
+import "regexp"
+
+var (
+	chineseRegex = regexp.MustCompile("[\u4e00-\u9fa5]")
+	englishRegex = regexp.MustCompile(`[a-zA-Z]+`)
+)
+
+func WordCount(text string) (int, int) {
+	chineseCount := len(chineseRegex.FindAllString(text, -1))
+	englishCount := len(englishRegex.FindAllString(text, -1))
+	return chineseCount, englishCount
+}
